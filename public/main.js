ca059f787076177866db0e0525634fd09e3d80ed
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-07 16:50:24 -0400

    Finished with explorer mode

--- public/main.js
+++ public/main.js
@@ -2,5 +2,24 @@ const main = () => {
   // document.querySelector('h1').textContent += '?'
 }
 
-document.addEventListener('DOMContentLoaded', main)
-document.querySelector('.weather-button').addEventListener('click', weatherButtonClickEvent)
+const weatherButton = () => {
+  // console.log('Weather Button is being clicked')
+  const userInput = document.querySelector('.input').value
+  console.log(userInput)
+
+  const _url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=00234cebb5770bb47207fd9f52bd28c3'
+
+  fetch(_url)
+    .then(Response => {
+      console.log('Response = ', Response)
+      if (Response.status === 200) {
+        return Response.json()
+      } else {
+        console.log('error', Response)
+      }
+    }).then(searchResults => {
+      console.log('Search Results = ', searchResults)
+    })
+}
+
+document.querySelector('.weather-button').addEventListener('click', weatherButton)
