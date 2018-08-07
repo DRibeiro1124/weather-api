cb59d806c6c53af698423bb28acebadfbd655e98
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-07 13:08:43 -0400

    inital commit with input/button section

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  // document.querySelector('h1').textContent += '?'
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.weather-button').addEventListener('click', weatherButtonClickEvent)
