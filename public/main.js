1683a5232e66e42e30843d84d3b6d14b8668a927
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-07 20:10:48 -0400

    switched temp from Kelvin to Farenheit

--- public/main.js
+++ public/main.js
@@ -2,12 +2,14 @@ const main = () => {
   // document.querySelector('h1').textContent += '?'
 }
 
+
+
 const weatherButton = () => {
   // console.log('Weather Button is being clicked')
   const userInput = document.querySelector('.input').value
-  console.log(userInput)
+  // console.log(userInput)
 
-  const _url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=00234cebb5770bb47207fd9f52bd28c3'
+  const _url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial' + '&appid=00234cebb5770bb47207fd9f52bd28c3'
 
   fetch(_url)
     .then(Response => {
@@ -17,8 +19,12 @@ const weatherButton = () => {
       } else {
         console.log('error', Response)
       }
-    }).then(searchResults => {
-      console.log('Search Results = ', searchResults)
+    }).then(weatherResult => {
+      console.log('Search Results = ', weatherResult)
+      const parent = document.querySelector('.searchResults')
+      const newLi = document.createElement('li')
+      newLi.textContent = weatherResult.main.temp
+      parent.appendChild(newLi)
     })
 }
 
